f97690982993fb9322eab603dc543484fe9bb435
Liyan2145 <Liyan.Yang21@student.xjtlu.edu.cn>
2024-11-23 11:26:34 +0800

    Carr generic_functions.R

--- R/generic_functions.R
+++ R/generic_functions.R
@@ -1,39 +1,39 @@
 #' @export
-print.GarchMidas <- function(x, ...) {
-  if (class(x) != "GarchMidas") {
-      stop("Obejct is not in class GarchMidas")
+print.CarrMidas <- function(x, ...) {
+  if (class(x) != "CarrMidas") {
+      stop("Obejct is not in class CarrMidas")
   }
   print(x$estimation)
 }
 
 #' @importFrom graphics lines
 #' @export
-plot.GarchMidas <- function(x, ...) {
-  if (class(x) != "GarchMidas") {
-    stop("Obejct is not in class GarchMidas")
+plot.CarrMidas <- function(x, ...) {
+  if (class(x) != "CarrMidas") {
+    stop("Obejct is not in class CarrMidas")
   }
 
   df_plot <- x$df.fitted[which(!is.na(x$df.fitted$g)),]
-  plot(x = df_plot[, 1], y = sqrt(252*df_plot$g*df_plot$tau),
+  plot(x = df_plot[, 1], y = 252*df_plot$h*df_plot$tau,
        type = "l",
        xlab = colnames(x$df.fitted)[3], ylab = "vol",
-       main = "sqrt(tau * g) and sqrt(tau) in red", sub = "")
+       main = "tau * h and tau in red", sub = "")
   lines(x = df_plot[, 1],
-        y = sqrt(252*df_plot$tau),
+        y = 252*df_plot$tau,
         col = "red")
 }
 
 #' @export
-predict.GarchMidas <- function(x,date, ...) {
-  if (class(x) != "GarchMidas") {
-    stop("Obejct is not in class GarchMidas")
+predict.CarrMidas <- function(x,date, ...) {
+  if (class(x) != "CarrMidas") {
+    stop("Obejct is not in class CarrMidas")
   }
   index <- which(x$df.fitted$date>=as.Date(date))[1]
   fitted <- x$df.fitted[(index-1):dim(x$df.fitted)[1],]
   alpha <- x$par["alpha"]
   beta <- x$par["beta"]
 
-  result<-fitted$tau*(1-alpha-beta+beta*fitted$g)+alpha*(fitted[,2]-x$par["mu"])^2
+  result<-fitted$tau*(1-alpha-beta+beta*fitted$h)+alpha*(fitted[,2])
   result[-length(result)]/10000
 }
 
@@ -42,8 +42,8 @@ predict.GarchMidas <- function(x,date, ...) {
 #' @importFrom graphics plot
 #' @export
 plot_weighting_scheme <- function(x) {
-  if (class(x) != "GarchMidas") {
-    stop("Obejct is not in class GarchMidas")
+  if (class(x) != "CarrMidas") {
+    stop("Obejct is not in class CarrMidas")
   }
   plot(c(1:x$K), x$phi)
 }
@@ -62,12 +62,12 @@ plot_weighting_scheme <- function(x) {
 #' @importFrom parallel stopCluster
 caculate_llh <- function(data, y, x, freq = "month", K.seq = c(6:48)) {
   fun <- function(k){
-    result <-fit_GarchMidas(data,y,x,k,freq)
+    result <-fit_CarrMidas(data,y,x,k,freq)
     result$llh
   }
   clnum<-detectCores(logical = FALSE)
   cl<-makeCluster(getOption('cl.cores',clnum))
-  clusterEvalQ(cl, library(GarchMidas))
+  clusterEvalQ(cl, library(CarrMidas))
   llhs<-parLapply(cl,K.seq,fun)
   stopCluster(cl)
   llhs
